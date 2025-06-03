e26f694e9733b09f157f5117af49db09b76e7d07
Korz <julian.klecha@gmx.de>
2025-06-03 16:59:47 +0200

    - implemented error handling in hub controller.

--- source/Controller/HubController.cs
+++ source/Controller/HubController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using TrialOfCrusaders.Enums;
 using TrialOfCrusaders.Manager;
 using TrialOfCrusaders.Resources.Text;
-using TrialOfCrusaders.UnityComponents.Debuffs;
 using TrialOfCrusaders.UnityComponents.Other;
 using TrialOfCrusaders.UnityComponents.StageElements;
 using UnityEngine;
@@ -28,10 +27,10 @@ internal static class HubController
     {
         if (_enabled)
             return;
-        LogHelper.Write<TrialOfCrusaders>("Enable Hub Controller", KorzUtils.Enums.LogType.Debug);
-        On.PlayMakerFSM.OnEnable += PlayMakerFSM_OnEnable;
-        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
-        On.GameManager.BeginSceneTransition += GameManager_BeginSceneTransition;
+        LogManager.Log("Enable Hub Controller");
+        On.PlayMakerFSM.OnEnable += FsmEdits;
+        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += SceneChanged;
+        On.GameManager.BeginSceneTransition += ModifySceneTransition;
 
         _enabled = true;
     }
@@ -40,10 +39,10 @@ internal static class HubController
     {
         if (!_enabled)
             return;
-        LogHelper.Write<TrialOfCrusaders>("Disable Hub Controller", KorzUtils.Enums.LogType.Debug);
-        On.PlayMakerFSM.OnEnable -= PlayMakerFSM_OnEnable;
-        UnityEngine.SceneManagement.SceneManager.activeSceneChanged -= SceneManager_activeSceneChanged;
-        On.GameManager.BeginSceneTransition -= GameManager_BeginSceneTransition;
+        LogManager.Log("Disable Hub Controller");
+        On.PlayMakerFSM.OnEnable -= FsmEdits;
+        UnityEngine.SceneManagement.SceneManager.activeSceneChanged -= SceneChanged;
+        On.GameManager.BeginSceneTransition -= ModifySceneTransition;
         _enabled = false;
     }
 
@@ -53,44 +52,51 @@ internal static class HubController
 
     private static void SetupTransitions()
     {
-        List<TransitionPoint> transitions = [.. Object.FindObjectsOfType<TransitionPoint>()];
-        TransitionPoint left = transitions.First(x => x.name == "left1");
-        GameObject block = new("Block");
-        block.SetActive(true);
-        block.transform.position = new(14.5f, 9);
-        block.AddComponent<BoxCollider2D>().size = new(1f, 8f);
-        transitions.Remove(left);
-        Object.Destroy(left);
-        transitions.First(x => x.name == "bot1").targetScene = "Dream_Room_Believer_Shrine";
-        CoroutineHelper.WaitForHero(() =>
+        try
         {
-            // Reset data
-            PDHelper.HasDash = false;
-            PDHelper.CanDash = false;
-            PDHelper.HasShadowDash = false;
-            PDHelper.HasWalljump = false;
-            PDHelper.CanWallJump = false;
-            PDHelper.HasDoubleJump = false;
-            PDHelper.HasLantern = false;
-            PDHelper.HasSuperDash = false;
-            PDHelper.HasAcidArmour = false;
-            PDHelper.FireballLevel = 0;
-            PDHelper.QuakeLevel = 0;
-            PDHelper.Geo = 0;
-            PDHelper.DreamOrbs = 0;
-            PDHelper.HasDreamNail = true;
-            PDHelper.MaxHealth = 5;
-            PDHelper.SoulLimited = false;
-            PDHelper.GrimmChildLevel = 0;
-            PlayMakerFSM.BroadcastEvent("UPDATE NAIL DAMAGE");
-            GameCameras.instance.hudCanvas.gameObject.SetActive(false);
-            GameCameras.instance.hudCanvas.gameObject.SetActive(true);
-            PDHelper.Geo = 0;
-            GameObject.Find("_GameCameras").transform.Find("HudCamera/Hud Canvas/Geo Counter").gameObject
-                .LocateMyFSM("Geo Counter")
-                .SendEvent("TO ZERO");
-            GameManager.instance.SaveGame();
-        }, true);
+            List<TransitionPoint> transitions = [.. Object.FindObjectsOfType<TransitionPoint>()];
+            TransitionPoint left = transitions.First(x => x.name == "left1");
+            GameObject block = new("Block");
+            block.SetActive(true);
+            block.transform.position = new(14.5f, 9);
+            block.AddComponent<BoxCollider2D>().size = new(1f, 8f);
+            transitions.Remove(left);
+            Object.Destroy(left);
+            transitions.First(x => x.name == "bot1").targetScene = "Dream_Room_Believer_Shrine";
+            CoroutineHelper.WaitForHero(() =>
+            {
+                // Reset data
+                PDHelper.HasDash = false;
+                PDHelper.CanDash = false;
+                PDHelper.HasShadowDash = false;
+                PDHelper.HasWalljump = false;
+                PDHelper.CanWallJump = false;
+                PDHelper.HasDoubleJump = false;
+                PDHelper.HasLantern = false;
+                PDHelper.HasSuperDash = false;
+                PDHelper.HasAcidArmour = false;
+                PDHelper.FireballLevel = 0;
+                PDHelper.QuakeLevel = 0;
+                PDHelper.Geo = 0;
+                PDHelper.DreamOrbs = 0;
+                PDHelper.HasDreamNail = true;
+                PDHelper.MaxHealth = 5;
+                PDHelper.SoulLimited = false;
+                PDHelper.GrimmChildLevel = 0;
+                PlayMakerFSM.BroadcastEvent("UPDATE NAIL DAMAGE");
+                GameCameras.instance.hudCanvas.gameObject.SetActive(false);
+                GameCameras.instance.hudCanvas.gameObject.SetActive(true);
+                PDHelper.Geo = 0;
+                GameObject.Find("_GameCameras").transform.Find("HudCamera/Hud Canvas/Geo Counter").gameObject
+                    .LocateMyFSM("Geo Counter")
+                    .SendEvent("TO ZERO");
+                GameManager.instance.SaveGame();
+            }, true);
+        }
+        catch (System.Exception ex)
+        {
+            LogManager.Log("Failed to setup hub transitions.", ex);
+        }
     } 
 
     #endregion
@@ -100,106 +106,127 @@ internal static class HubController
     /// <summary>
     /// Controls all FSM related modifications.
     /// </summary>
-    private static void PlayMakerFSM_OnEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
+    private static void FsmEdits(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
     {
-        if (self.gameObject.name == "Little Fool NPC" && self.FsmName == "Conversation Control")
+        try
         {
-            self.GetState("Opened?").AdjustTransition("FINISHED", "Gate Opened");
-            self.GetState("Gate Opened").InsertActions(0, () =>
+            if (self.gameObject.name == "Little Fool NPC" && self.FsmName == "Conversation Control")
             {
-                // Select dialog.
-                GameHelper.OneTimeMessage("LITTLE_FOOL_CHALLENGE", LittleFoolDialog.Greeting, "Minor NPC");
-            });
+                self.GetState("Opened?").AdjustTransition("FINISHED", "Gate Opened");
+                self.GetState("Gate Opened").InsertActions(0, () =>
+                {
+                    // Select dialog.
+                    GameHelper.OneTimeMessage("LITTLE_FOOL_CHALLENGE", LittleFoolDialog.Greeting, "Minor NPC");
+                });
+            }
+        }
+        catch (System.Exception ex)
+        {
+            LogManager.Log("Failed to modify hub fsm edits.", ex);
         }
         orig(self);
     }
 
-    private static void GameManager_BeginSceneTransition(On.GameManager.orig_BeginSceneTransition orig, GameManager self, GameManager.SceneLoadInfo info)
+    private static void ModifySceneTransition(On.GameManager.orig_BeginSceneTransition orig, GameManager self, GameManager.SceneLoadInfo info)
     {
-        if (info.SceneName == "Dream_Room_Believer_Shrine")
-            info.EntryGateName = "left1";
-        else if (info.SceneName == "Dream_Backer_Shrine")
+        try
         {
-            info.SceneName = "Room_Colosseum_01";
-            info.EntryGateName = "bot1";
+            if (info.SceneName == "Dream_Room_Believer_Shrine")
+                info.EntryGateName = "left1";
+            else if (info.SceneName == "Dream_Backer_Shrine")
+            {
+                info.SceneName = "Room_Colosseum_01";
+                info.EntryGateName = "bot1";
+            }
+            else if (info.SceneName == "Select Target")
+            {
+                info.SceneName = "GG_Spa";
+                info.EntryGateName = "door_dreamEnter";
+                int finalSeed = int.Parse(string.Join("", _seedTablets.Select(x => x.Number.ToString())));
+                RngManager.Seeded = finalSeed != _rolledSeed;
+                RngManager.Seed = finalSeed;
+                StageController.CurrentRoomData = SetupManager.GenerateNormalRun();
+                StageController.CurrentRoomIndex = -1;
+                PhaseController.TransitionTo(Phase.Run);
+            }
+            else if (info.SceneName == "Room_Colosseum_Bronze")
+            {
+                PDHelper.HasDreamNail = false;
+                info.SceneName = "Deepnest_East_10";
+                info.EntryGateName = "left1";
+            }
         }
-        else if (info.SceneName == "Select Target")
+        catch (System.Exception ex)
         {
-            info.SceneName = "GG_Spa";
-            info.EntryGateName = "door_dreamEnter";
-            int finalSeed = int.Parse(string.Join("", _seedTablets.Select(x => x.Number.ToString())));
-            RngManager.Seeded = finalSeed != _rolledSeed;
-            RngManager.Seed = finalSeed;
-            StageController.CurrentRoomData = SetupManager.GenerateNormalRun();
-            StageController.CurrentRoomIndex = -1;
-            PhaseController.TransitionTo(Phase.Run);
-        }
-        else if (info.SceneName == "Room_Colosseum_Bronze")
-        {
-            PDHelper.HasDreamNail = false;
-            info.SceneName = "Deepnest_East_10";
-            info.EntryGateName = "left1";
+            LogManager.Log("Error in hub modify scene transition.", ex);
         }
         orig(self, info);
     }
 
-    private static void SceneManager_activeSceneChanged(UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.Scene arg1)
+    private static void SceneChanged(UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.Scene arg1)
     {
-        if (arg1.name == "Room_Colosseum_01")
-            SetupTransitions();
-        else if (arg1.name == "Deepnest_East_10")
+        try
         {
-            _seedTablets.Clear();
-            Object.Destroy(GameObject.Find("plat_float_05 (1)"));
-            Object.Destroy(GameObject.Find("plat_float_05"));
-            Object.Destroy(GameObject.Find("white_ash_scenery_0004_5 (3)"));
-            Object.Destroy(GameObject.Find("Inspect Region Ghost"));
-            _rolledSeed = Random.Range(100000000, 1000000000);
-            float xPosition = 18.2f;
-            float yPosition = 9.5f;
-            for (int i = 0; i < 9; i++)
+            if (arg1.name == "Room_Colosseum_01")
+                SetupTransitions();
+            else if (arg1.name == "Deepnest_East_10")
             {
-                // 18.2f
-                // 21.7f, 9.5f
-                // 25.2f, 9.5f
-                // 4.9f or 9.5f Y
-                GameObject obstacleGameObject = new("Sign");
-                obstacleGameObject.SetActive(false);
-                obstacleGameObject.transform.position = new(xPosition, yPosition, -0.08f);
-                obstacleGameObject.transform.localScale = new(2f, 2f, 1f);
-                obstacleGameObject.AddComponent<SpriteRenderer>().sprite = SpriteHelper.CreateSprite<TrialOfCrusaders>("Sprites.Other.Mirror");
-                obstacleGameObject.AddComponent<BoxCollider2D>().size = new(1f, 1f);
-                obstacleGameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-                obstacleGameObject.AddComponent<TinkEffect>().blockEffect = Tink;
-                obstacleGameObject.layer = 11;
-                SeedTablet tablet = obstacleGameObject.AddComponent<SeedTablet>();
-                _seedTablets.Add(tablet);
-                tablet.Index = i;
-                tablet.Number = int.Parse("" + _rolledSeed.ToString()[i]);
-
-                GameObject abilitySprite = new("Ability Sprite");
-                abilitySprite.transform.SetParent(obstacleGameObject.transform);
-                abilitySprite.transform.localPosition = new(0f, -0.1f, -0.01f);
-                abilitySprite.transform.localScale = new(.28f, .28f);
-                abilitySprite.AddComponent<SpriteRenderer>();
-                abilitySprite.SetActive(true);
-                obstacleGameObject.SetActive(true);
-                xPosition += 3.5f;
-                if (xPosition == 25.2f && yPosition == 4.9f)
-                    xPosition += 3.5f;
-                else if (xPosition > 32.5)
+                _seedTablets.Clear();
+                Object.Destroy(GameObject.Find("plat_float_05 (1)"));
+                Object.Destroy(GameObject.Find("plat_float_05"));
+                Object.Destroy(GameObject.Find("white_ash_scenery_0004_5 (3)"));
+                Object.Destroy(GameObject.Find("Inspect Region Ghost"));
+                _rolledSeed = Random.Range(100000000, 1000000000);
+                float xPosition = 18.2f;
+                float yPosition = 9.5f;
+                for (int i = 0; i < 9; i++)
                 {
-                    yPosition -= 4.6f;
-                    xPosition = 18.2f;
+                    // 18.2f
+                    // 21.7f, 9.5f
+                    // 25.2f, 9.5f
+                    // 4.9f or 9.5f Y
+                    GameObject obstacleGameObject = new("Sign");
+                    obstacleGameObject.SetActive(false);
+                    obstacleGameObject.transform.position = new(xPosition, yPosition, -0.08f);
+                    obstacleGameObject.transform.localScale = new(2f, 2f, 1f);
+                    obstacleGameObject.AddComponent<SpriteRenderer>().sprite = SpriteHelper.CreateSprite<TrialOfCrusaders>("Sprites.Other.Mirror");
+                    obstacleGameObject.AddComponent<BoxCollider2D>().size = new(1f, 1f);
+                    obstacleGameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+                    obstacleGameObject.AddComponent<TinkEffect>().blockEffect = Tink;
+                    obstacleGameObject.layer = 11;
+                    SeedTablet tablet = obstacleGameObject.AddComponent<SeedTablet>();
+                    _seedTablets.Add(tablet);
+                    tablet.Index = i;
+                    tablet.Number = int.Parse("" + _rolledSeed.ToString()[i]);
+
+                    GameObject abilitySprite = new("Ability Sprite");
+                    abilitySprite.transform.SetParent(obstacleGameObject.transform);
+                    abilitySprite.transform.localPosition = new(0f, -0.1f, -0.01f);
+                    abilitySprite.transform.localScale = new(.28f, .28f);
+                    abilitySprite.AddComponent<SpriteRenderer>();
+                    abilitySprite.SetActive(true);
+                    obstacleGameObject.SetActive(true);
+                    xPosition += 3.5f;
+                    if (xPosition == 25.2f && yPosition == 4.9f)
+                        xPosition += 3.5f;
+                    else if (xPosition > 32.5)
+                    {
+                        yPosition -= 4.6f;
+                        xPosition = 18.2f;
+                    }
                 }
+                CoroutineHelper.WaitForHero(() =>
+                {
+                    GameObject startTransition = new("Start Transition");
+                    SpecialTransition transition = startTransition.AddComponent<SpecialTransition>();
+                    transition.VanillaTransition = GameObject.Find("left1").GetComponent<TransitionPoint>();
+                    transition.LoadIntoDream = true;
+                }, true);
             }
-            CoroutineHelper.WaitForHero(() =>
-            {
-                GameObject startTransition = new("Start Transition");
-                SpecialTransition transition = startTransition.AddComponent<SpecialTransition>();
-                transition.VanillaTransition = GameObject.Find("left1").GetComponent<TransitionPoint>();
-                transition.LoadIntoDream = true;
-            }, true);
+        }
+        catch (System.Exception ex)
+        {
+            LogManager.Log("Error on hub scene changed.", ex);
         }
     } 
 
